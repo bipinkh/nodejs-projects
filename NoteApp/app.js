e030c10788b8076ccb63f9a8007a65ef8f1523cf
bipinkh <bipinkh91@gmail.com>
2018-05-19 19:44:25 +0545

    cleaned more dirt

--- NoteApp/app.js
+++ NoteApp/app.js
@@ -1,6 +1,7 @@
 console.log("Starting app.js");
+
 const fs = require("fs");
-const os = require("os");
 const _ = require("lodash");
+
 const notes = require("./notes.js");
 
--- NoteApp/notes.js
+++ NoteApp/notes.js
@@ -1,14 +1,8 @@
 console.log("Starting notes.js");
-//console.log(module);
-
-module.exports.age = 25;
 
 //module.exports.addNote = function(){}
+
 module.exports.addNote = () => {
     console.log("add note function is called");
     return "New note.";
-}
-
-module.exports.addNumbers = (a, b) => {
-    return (a+b);
 }
