b4541121b38d6e2bffba405677b2162ed28284f0
bipinkh <bipinkh91@gmail.com>
2018-05-21 08:52:20 +0545

    reading command line arguments

--- NoteApp/app.js
+++ NoteApp/app.js
@@ -5,3 +5,24 @@ const _ = require("lodash");
 
 const notes = require("./notes.js");
 
+//list all the arguments
+console.log(process.argv);
+
+var command = process.argv[2];
+console.log(`command : ${command}`);
+
+if (command == 'add'){
+    console.log(`adding new note`);
+    
+}else if (command == 'list'){
+    console.log(`listing all notes`);
+
+}else if (command == 'read'){
+    console.log(`fetching note`);
+
+}else if (command == 'remove'){
+    console.log('removing note');
+
+}else {
+    console.log('commad not recognized');
+}
